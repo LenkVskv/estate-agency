fb8105bb2ab71f65b89733e907fcb59ace1366d0
OlegDemkiv <olegdemkiv@gmail.com>
2020-03-29 00:31:02 +0200

    Fixed JavaScript code with ESLint

--- js/header.js
+++ js/header.js
@@ -1,56 +1,55 @@
-var button = document.querySelector(".myButton");
-   if(button) button.addEventListener("click", function userSection(){
-   document.querySelector(".user").style.display= document.querySelector(".user").style.display == "block" ? "none" : "block";
-});
-function onResize(){
-   var button = document.querySelector(".menu-btn");
-   if(button) button.style.display = window.innerWidth <= 750 ? 'block' : 'none';
+var button = document.querySelector('.myButton');
+if (button) {
+  button.addEventListener('click', function userSection() {
+    document.querySelector('.user').style.display= document.querySelector('.user').style.display == 'block' ? 'none' : 'block';
+  });
+}
+function onResize() {
+  const button = document.querySelector('.menu-btn');
+  if (button) button.style.display = window.innerWidth <= 750 ? 'block' : 'none';
 }
 window.addEventListener('resize', onResize);
 onResize();
 
 
- function mainorder(){
-   let account = JSON.parse(localStorage.getItem("account"));
-  let login = $("#signIn");
-  let logout = $("#logout-button");
-  let user = $("#user_name");
-  if(account !== null){
-   user.html(account.firstName);
-   login.hide();
-  logout.show();
-  user.show();
+function mainorder() {
+  const account = JSON.parse(localStorage.getItem('account'));
+  const login = $('#signIn');
+  const logout = $('#logout-button');
+  const user = $('#user_name');
+  if (account !== null) {
+    user.html(account.firstName);
+    login.hide();
+    logout.show();
+    user.show();
+  } else {
+    login.show();
+    logout.hide();
+    user.hide();
   }
-  else{
-   login.show();
-   logout.hide();
-   user.hide();
- }
-   logout.click((event)=>{
-     event.preventDefault();
-     localStorage.removeItem("account");
-     mainorder();
- 
-   })
- }
-// 
+  logout.click((event)=>{
+    event.preventDefault();
+    localStorage.removeItem('account');
+    mainorder();
+  });
+}
+//
 $( document ).ready(function() {
   $('#login-button').click((event) =>{
     event.preventDefault();
     let signAccount = null;
-    let firstName = $("#log-login").val();
-    let pass = $("#log-password").val();
-    let accounts = JSON.parse(localStorage.getItem("accounts"));
-    accounts.forEach(acc => {
-      if(acc.firstName === firstName && acc.pass == pass){
+    const firstName = $('#log-login').val();
+    const pass = $('#log-password').val();
+    const accounts = JSON.parse(localStorage.getItem('accounts'));
+    accounts.forEach((acc) => {
+      if (acc.firstName === firstName && acc.pass == pass) {
         signAccount = acc;
       }
     });
-    if(signAccount){
-      localStorage.setItem("account", JSON.stringify(signAccount))
-      window.location.href = "main.html";
+    if (signAccount) {
+      localStorage.setItem('account', JSON.stringify(signAccount));
+      window.location.href = 'main.html';
     }
-  })
+  });
   mainorder();
-  
 });
--- js/property.js
+++ js/property.js
@@ -14,132 +14,132 @@
 // $("[name=value]").val(value);
 const flats = [
   {
-    name: "Gorgeous Apartment bay view",
-    imageSrc:"/image/flats/apartament-2.jpg",
-    price:5000,
-    type:"rent",
-    district:"holosiivsky"
+    name: 'Gorgeous Apartment bay view',
+    imageSrc: '/image/flats/apartament-2.jpg',
+    price: 5000,
+    type: 'rent',
+    district: 'holosiivsky',
 
   },
   {
-    name: "Contemporary apartment ",
-    imageSrc:"/image/flats/apartament-3.jpg",
-    price:450,
-    type:"buy",
-    district:"shevchenkivsky"
+    name: 'Contemporary apartment ',
+    imageSrc: '/image/flats/apartament-3.jpg',
+    price: 450,
+    type: 'buy',
+    district: 'shevchenkivsky',
 
   },
   {
-    name: "Modern apartment on the bay ",
-    imageSrc:"/image/flats/apartament-4.jpg",
-    price:4500,
-    type:"buy",
-    district:"pechersʹkyy"
+    name: 'Modern apartment on the bay ',
+    imageSrc: '/image/flats/apartament-4.jpg',
+    price: 4500,
+    type: 'buy',
+    district: 'pechersʹkyy',
 
   },
   {
-    name: "ЖК LIKOGRAD Perfect Town ",
-    imageSrc:"/image/flats/apartament-5.jpg",
-    price:700,
-    type:"rent",
-    district:"solom'yansʹkyy"
+    name: 'ЖК LIKOGRAD Perfect Town ',
+    imageSrc: '/image/flats/apartament-5.jpg',
+    price: 700,
+    type: 'rent',
+    district: 'solom\'yansʹkyy',
 
   },
   {
-    name: "17, ParkLand apartment",
-    imageSrc:"/image/flats/apartament-6.jpg",
-    price:500,
-    type:"sold",
-    district:"holosiivsky"
+    name: '17, ParkLand apartment',
+    imageSrc: '/image/flats/apartament-6.jpg',
+    price: 500,
+    type: 'sold',
+    district: 'holosiivsky',
 
   },
   {
-    name: "Specific penthouse",
-    imageSrc:"/image/flats/apartament-7.jpg",
-    price:80000,
-    type:"buy",
-    district:"shevchenkivsky"
+    name: 'Specific penthouse',
+    imageSrc: '/image/flats/apartament-7.jpg',
+    price: 80000,
+    type: 'buy',
+    district: 'shevchenkivsky',
 
   },
   {
-    name: "For sale 2-room view apartment",
-    imageSrc:"/image/flats/apartament-8.jpg",
-    price:65000,
-    type:"buy",
-    district:"svyatoshinsky"
+    name: 'For sale 2-room view apartment',
+    imageSrc: '/image/flats/apartament-8.jpg',
+    price: 65000,
+    type: 'buy',
+    district: 'svyatoshinsky',
 
   },
   {
-    name: "New, designer apartment",
-    imageSrc:"/image/flats/apartament-9.jpg",
-    price:500,
-    type:"rent",
-    district:"pechersʹkyy"
+    name: 'New, designer apartment',
+    imageSrc: '/image/flats/apartament-9.jpg',
+    price: 500,
+    type: 'rent',
+    district: 'pechersʹkyy',
 
   },
   {
-    name: "New, designer apartment",
-    imageSrc:"/image/flats/apartament-9.jpg",
-    price:500,
-    type:"rent",
-    district:"pechersʹkyy"
+    name: 'New, designer apartment',
+    imageSrc: '/image/flats/apartament-9.jpg',
+    price: 500,
+    type: 'rent',
+    district: 'pechersʹkyy',
 
   },
   {
-    name: "Smart house",
-    imageSrc:"/image/flats/apartament-10.jpg",
-    price:300,
-    type:"sold",
-    district:"svyatoshinsky"
+    name: 'Smart house',
+    imageSrc: '/image/flats/apartament-10.jpg',
+    price: 300,
+    type: 'sold',
+    district: 'svyatoshinsky',
 
   },
   {
-    name: "Long-term apartment rental",
-    imageSrc:"/image/flats/apartament-11.jpg",
-    price:450,
-    type:"rent",
-    district:"holosiivsky"
+    name: 'Long-term apartment rental',
+    imageSrc: '/image/flats/apartament-11.jpg',
+    price: 450,
+    type: 'rent',
+    district: 'holosiivsky',
 
   },
   {
-    name: "Venice First time for rent!",
-    imageSrc:"/image/flats/apartament-11.jpg",
-    price:600,
-    type:"rent",
-    district:"pechersʹkyy"
+    name: 'Venice First time for rent!',
+    imageSrc: '/image/flats/apartament-11.jpg',
+    price: 600,
+    type: 'rent',
+    district: 'pechersʹkyy',
   },
   {
-    name: "High-end designer renovation",
-    imageSrc:"/image/flats/apartament-13.jpg",
-    price:30000,
-    type:"buy",
-    district:"solom'yansʹkyy"
+    name: 'High-end designer renovation',
+    imageSrc: '/image/flats/apartament-13.jpg',
+    price: 30000,
+    type: 'buy',
+    district: 'solom\'yansʹkyy',
   },
   {
-    name: "Bright warm apartment.",
-    imageSrc:"/image/flats/apartament-14.jpg",
-    price:350,
-    type:"rent",
-    district:"shevchenkivsky"
+    name: 'Bright warm apartment.',
+    imageSrc: '/image/flats/apartament-14.jpg',
+    price: 350,
+    type: 'rent',
+    district: 'shevchenkivsky',
   },
   {
-    name: "WITHOUT COMMISSION! Three-room apartment for sale.",
-    imageSrc:"/image/flats/apartament-15.jpg",
-    price:95000,
-    type:"sold",
-    district:"svyatoshinsky"
+    name: 'WITHOUT COMMISSION! Three-room apartment for sale.',
+    imageSrc: '/image/flats/apartament-15.jpg',
+    price: 95000,
+    type: 'sold',
+    district: 'svyatoshinsky',
   },
   {
-    name: "Sale of an apartment in a new house.",
-    imageSrc:"/image/flats/apartament-16.jpg",
-    price:45000,
-    type:"buy",
-    district:"holosiivsky"
+    name: 'Sale of an apartment in a new house.',
+    imageSrc: '/image/flats/apartament-16.jpg',
+    price: 45000,
+    type: 'buy',
+    district: 'holosiivsky',
   },
-  
+
 
 ];
-function generator(flat){
+function generator(flat) {
   return `<div class="product-box__item ">
                   <h3 class="product-box__title">${flat.name} </h3>
                   <div class="product-box__img">
@@ -151,26 +151,23 @@ function generator(flat){
                       <p>$${flat.price}</p>
                       <button class="product-box__btn">Buy</button>
                   </div>
-                </div>`
-
+                </div>`;
 }
-function updateFlatsContent(){
-  var flatsContainer = document.querySelector("#flats");
-  var flatsContent = "";
-  var localFlats = flats.slice(0);
-  var flatType = document.querySelector("#flat_type").value;
-  var flatPrice = document.querySelector("#price").value;
-  var flatDistrict = document.querySelector("#district").value;
-  localFlats = localFlats.filter(flat => {
-    return (flatType != 'all' ? flat.type === flatType : true) && 
-            (flatPrice != '' ? flat.price <= Number(flatPrice) : true) && 
-            (flatDistrict != 'all' ? flat.district === flatDistrict : true)
-  })
-  localFlats.forEach(flat => flatsContent+=generator(flat));
+function updateFlatsContent() {
+  const flatsContainer = document.querySelector('#flats');
+  let flatsContent = '';
+  let localFlats = flats.slice(0);
+  const flatType = document.querySelector('#flat_type').value;
+  const flatPrice = document.querySelector('#price').value;
+  const flatDistrict = document.querySelector('#district').value;
+  localFlats = localFlats.filter((flat) => {
+    return (flatType != 'all' ? flat.type === flatType : true) &&
+            (flatPrice != '' ? flat.price <= Number(flatPrice) : true) &&
+            (flatDistrict != 'all' ? flat.district === flatDistrict : true);
+  });
+  localFlats.forEach((flat) => flatsContent+=generator(flat));
   flatsContainer.innerHTML = flatsContent;
 }
-document.addEventListener('DOMContentLoaded', function(){ 
-
+document.addEventListener('DOMContentLoaded', function() {
   updateFlatsContent();
-
 });
